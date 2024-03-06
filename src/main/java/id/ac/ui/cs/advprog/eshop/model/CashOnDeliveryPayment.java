8dcacc05b09df0c3f9c364fca6d425d2954ded52
asteriskzie <ester.gracia.10@gmail.com>
2024-03-06 20:28:38 +0700

    [REFACTOR] Create not null or empty string checker method for CashOnDeliveryPayment data validation

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/CashOnDeliveryPayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/CashOnDeliveryPayment.java
@@ -5,11 +5,15 @@ import enums.PaymentStatus;
 import java.util.Map;
 
 public class CashOnDeliveryPayment extends Payment {
+    private boolean notNullOrEmpty(String data) {
+        return data != null && !data.isEmpty();
+    }
+
     private boolean isValidData(Map<String,String> paymentData) {
         String address = paymentData.get("address");
         String deliveryFee = paymentData.get("deliveryFee");
 
-        return address != null && !address.isEmpty() && deliveryFee != null && !deliveryFee.isEmpty();
+        return notNullOrEmpty(address) && notNullOrEmpty(deliveryFee);
     }
     public CashOnDeliveryPayment(String paymentId, Map<String, String> paymentData) {
         super(paymentId, "CashOnDelivery", PaymentStatus.REJECTED.getValue(), paymentData);
